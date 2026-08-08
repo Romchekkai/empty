8b1422d05addaa1d53ad6bff18723c73caa25951
smartiqa <info@smartiqa.com>
2026-08-08 19:29:48 +0200

    new commit

--- project.sh
+++ project.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-SUCCESS=true
-
-echo "Запуск процесса..."
-
-if [ "$SUCCESS" = true ]; then
-    echo "Успех: Все задачи выполнены корректно!"
-    exit 0
-else
-    echo "Ошибка: Что-то пошло не так!" >&2
-    exit 1
-fi
+echo "Проверка запущена"
+exit 1
